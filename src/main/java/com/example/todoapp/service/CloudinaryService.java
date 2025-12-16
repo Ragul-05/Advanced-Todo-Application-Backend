cda892bcb77490eaa897b405d65a0c02cc4b43c7
Ragul-05 <ragulgopinath2005@gmail.com>
2025-12-16 09:29:32 +0530

    feat:Added and  implement profile service with image upload support to Cloudinary

--- src/main/java/com/example/todoapp/service/CloudinaryService.java
+++ src/main/java/com/example/todoapp/service/CloudinaryService.java
@@ -1,4 +1,45 @@
 package com.example.todoapp.service;
 
+import com.cloudinary.Cloudinary;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@Service
 public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    /* Upload Image */
+    public Map<String, String> uploadImage(MultipartFile file) {
+        try {
+            Map<?, ?> result = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    Map.of("folder", "todoapp/profile")
+            );
+
+            return Map.of(
+                    "url", result.get("secure_url").toString(),
+                    "publicId", result.get("public_id").toString()
+            );
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to upload image");
+        }
+    }
+
+    /* Delete Image */
+    public void deleteImage(String publicId) {
+        try {
+            cloudinary.uploader().destroy(publicId, Map.of());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to delete image");
+        }
+    }
 }
+
