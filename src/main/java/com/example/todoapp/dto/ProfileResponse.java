8c42371ca3184b533c6ef64472647a5f628f7f88
Ragul-05 <ragulgopinath2005@gmail.com>
2025-12-16 09:25:38 +0530

    feat: add login and register request/response DTO's

--- src/main/java/com/example/todoapp/dto/ProfileResponse.java
+++ src/main/java/com/example/todoapp/dto/ProfileResponse.java
@@ -1,4 +1,34 @@
 package com.example.todoapp.dto;
 
 public class ProfileResponse {
+
+    private String fullName;
+    private String email;
+    private String profileImage;
+
+    /* Getters & Setters */
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
 }
